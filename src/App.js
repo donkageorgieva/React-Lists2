b5b08315044c8be77913a6581992c874b7f84474
Donka Georgieva <65857282+donkageorgieva@users.noreply.github.com>
2021-02-25 14:36:35 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { React,Component } from 'react';
 import './App.css';
 import Input from './Input/Input';
 import Goal from './Goal/Goal';
+import styled from 'styled-components';
+import greenButton from './ButtonGreen/ButtonGreen.module.css'
 
 class App extends Component {
   state = {
@@ -40,6 +42,19 @@ class App extends Component {
  }
 
   render(){
+    const StyledDiv =     styled.div`
+    {
+      font-family: 'Helvetica', sans-serif;
+      font-size: 25px;
+      background-color: darksalmon;
+      width: 100%;
+      margin 0 auto;
+      text-align: center;
+      color: rgb(255, 239, 241);
+     
+    }
+    
+   `;
 const goals = this.state.goalList.map((goal, index) => {
   return <Goal goal = {goal} key={index} goalDone = {this.removeGoal.bind(this,goal)}/>
 })
@@ -57,17 +72,19 @@ if(this.state.goalList.length >0 && this.state.goalList.length > 2){
 
     return (
     
-      <div className="App">
-     
+  
+     <StyledDiv>
        <Input addGoal={this.addGoal.bind(this, this.state.currentGoal)} 
        currentGoal = {(e)=> this.setCurrentGoal(e)}  value={this.state.currentGoal} />
       {goals}
       <button className={classes.join(' ')}>Sup</button>
+      <button className = {greenButton.button}>Hi</button>
+  
 
    
-     
+      </StyledDiv>
        
-      </div>
+   
     );
   }
  
--- src/Goal/Goal.js
+++ src/Goal/Goal.js
@@ -1,12 +1,13 @@
 import { React} from 'react';
 import './Goal.css'
+import redButton from '../ButtonRed/ButtonRed.module.css'
 const goal = (props) => {
 
 return(
   
     <li>
     <p>{props.goal}</p>
-    <button onClick = {props.goalDone} >Done</button>
+    <button onClick = {props.goalDone} className={redButton.button}>Done</button>
    
     </li>
 )
